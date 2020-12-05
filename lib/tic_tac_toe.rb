06a7d503a3e802188d6e121c925b6f7cb79dd197
Rachid Abu-Hassan <rachid@platform78.com>
2020-12-05 15:03:51 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,6 +26,10 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+
   def move(location, character = "X")
     @board[location.to_i - 1] = character
   end
